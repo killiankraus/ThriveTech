2b9f1c2a51864f830abe3d4c0bc1cf7040b92f8f
earvintulagan <75780241+earvintulagan@users.noreply.github.com>
2021-01-23 01:05:10 +0800

    Add project files.

---
+++ sb-admin-2.Web/Controllers/HomeController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sb_admin_2.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MissionandVission()
+        {
+            return View();
+        }
+
+        public ActionResult CoreValues()
+        {
+            return View("CoreValues");
+        }
+
+        public ActionResult UniversityCalendar()
+        {
+            return View("UniversityCalendar");
+        }
+
+        public ActionResult UniversityHymn()
+        {
+            return View("UniversityHymn");
+        }
+
+        public ActionResult SanBartolome()
+        {
+            return View("San Bartolome");
+        }
+
+        public ActionResult Batasan()
+        {
+            return View("Batasan");
+        }
+
+        public ActionResult SanFrancisco()
+        {
+            return View("San Francisco");
+        }
+
+        public ActionResult TranscriptOfRecord()
+        {
+            return View("TranscriptOfRecord");
+        }
+
+        public ActionResult DownlableForms()
+        {
+            return View("Downloadable Forms");
+        }
+
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+
+        public ActionResult Register()
+        {
+            return View("Register");
+        }
+
+        public ActionResult ForgotPassword()
+        {
+            return View("ForgotPassword");
+        }
+
+
+        public JsonResult GetEvents()
+        {
+            using (RegistrationEntities dc = new RegistrationEntities())
+            {
+                var events = dc.Calendars.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+        [HttpPost]
+        public JsonResult SaveEvent(Calendar e)
+        {
+            var status = false;
+            using (RegistrationEntities dc = new RegistrationEntities())
+            {
+                if (e.EventID > 0)
+                {
+                    //Update the event
+                    var v = dc.Calendars.Where(a => a.EventID == e.EventID).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Subject = e.Subject;
+                        v.Start = e.Start;
+                        v.End = e.End;
+                        v.Description = e.Description;
+                        v.IsFullDay = e.IsFullDay;
+                        v.ThemeColor = e.ThemeColor;
+                    }
+                }
+                else
+                {
+                    dc.Calendars.Add(e);
+                }
+
+                dc.SaveChanges();
+                status = true;
+
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpPost]
+        public JsonResult DeleteEvent(int eventID)
+        {
+            var status = false;
+            using (RegistrationEntities dc = new RegistrationEntities())
+            {
+                var v = dc.Calendars.Where(a => a.EventID == eventID).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Calendars.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpPost]
+        public JsonResult AjaxMethod()
+        {
+            RegistrationEntities entities = new RegistrationEntities();
+            List<Member> members = (from account in entities.Members
+                                      select account).ToList();
+            return Json(members);
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ sb-admin-2.Web/Domain/Data.cs
@@ -0,0 +1,41 @@
+﻿using sb_admin_2.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sb_admin_2.Web.Domain
+{
+    public class Data
+    {
+        public IEnumerable<Navbar> navbarItems()
+        {
+            var menu = new List<Navbar>();
+            menu.Add(new Navbar { Id = 1, nameOption = "Dashboard", controller = "Home", action = "Index", imageClass = "fa fa-dashboard fa-fw", status = true, isParent = false, parentId = 0 });
+            menu.Add(new Navbar { Id = 2, nameOption = "General Information", imageClass = "fa fa-bar-chart-o fa-fw", status = true, isParent = true, parentId = 0 });
+            menu.Add(new Navbar { Id = 3, nameOption = "Mission & Vission", controller = "Home", action = "MissionandVission", status = true, isParent = false, parentId = 2 });
+            menu.Add(new Navbar { Id = 4, nameOption = "Core Values", controller = "Home", action = "CoreValues", status = true, isParent = false, parentId = 2 });
+            menu.Add(new Navbar { Id = 5, nameOption = "University Calendar ", controller = "Home", action = "UniversityCalendar", status = true, isParent = false, parentId = 2 });
+            menu.Add(new Navbar { Id = 6, nameOption = "University Hymn ", controller = "Home", action = "UniversityHymn", status = true, isParent = false, parentId = 2 });
+
+            menu.Add(new Navbar { Id = 7, nameOption = "Campuses", controller = "Home", action = "Campuses", imageClass = "fa fa-table fa-fw", status = true, isParent = true, parentId = 0 });
+            menu.Add(new Navbar { Id = 8, nameOption = "San Bartolome ", controller = "Home", action = "SanBartolome", status = true, isParent = false, parentId = 7 });
+            menu.Add(new Navbar { Id = 9, nameOption = "Batasan ", controller = "Home", action = "Batasan", status = true, isParent = false, parentId = 7 });
+            menu.Add(new Navbar { Id = 10, nameOption = "San Francisco ", controller = "Home", action = "SanFrancisco", status = false, isParent = false, parentId = 7 });
+
+            menu.Add(new Navbar { Id = 11, nameOption = "Records", controller = "Home", action = "Forms", imageClass = "fa fa-edit fa-fw", status = true, isParent = true, parentId = 0 });
+            menu.Add(new Navbar { Id = 12, nameOption = "List of Students", controller = "Home", action = "TranscriptOfRecord", status = false, isParent = false, parentId = 11 });
+            menu.Add(new Navbar { Id = 13, nameOption = "Downloadable forms ", controller = "Home", action = "DownloadableForms", status = false, isParent = false, parentId = 11 });
+
+            menu.Add(new Navbar { Id = 14, nameOption = "Pages", imageClass = "fa fa-wrench fa-fw", status = true, isParent = true, parentId = 0 });
+            menu.Add(new Navbar { Id = 15, nameOption = "Login", controller = "Home", action = "Login", status = true, isParent = false, parentId = 14 });
+            menu.Add(new Navbar { Id = 16, nameOption = "Register", controller = "Home", action = "Register", status = true, isParent = false, parentId = 14 });
+            menu.Add(new Navbar { Id = 17, nameOption = "Forgot Password", controller = "Home", action = "ForgotPassword", status = true, isParent = false, parentId = 14 });
+
+            menu.Add(new Navbar { Id = 18, nameOption = "Grades", imageClass = "fa fa-sitemap fa-fw", status = true, isParent = true, parentId = 0 });
+           
+
+            return menu.ToList();
+        }
+    }
+}
